5b1961d7eefddfa205f13b3f95bca49172de1edf
Apoorv Kumar <apoorvkumarvbcv@gmail.com>
2024-05-21 14:11:26 +0530

    setup

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import dotenv from "dotenv";
-if (process.env.NODE_ENV !== "production") dotenv.config();
-import express, { Express } from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
+import dotenv from 'dotenv';
+if (process.env.NODE_ENV !== 'production') dotenv.config();
+import express, { Express } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app: Express = express();
 
-app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(
-	bodyParser.urlencoded({
-		limit: "50mb",
-		extended: true,
-		parameterLimit: 50000,
-	})
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit: 50000,
+  }),
 );
 
 const port = process.env.PORT || 3000;
@@ -20,5 +20,5 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.listen(port, () => {
-	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
