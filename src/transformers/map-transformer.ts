76aec2570fa456377331cd6bb3f8681bb16f63d6
Apoorv Kumar <apoorvkumarvbcv@gmail.com>
2024-05-24 02:37:15 +0530

    added integration with db

--- src/transformers/map-transformer.ts
+++ src/transformers/map-transformer.ts
@@ -2,11 +2,13 @@ export const MapMatrixTransformer = (
   matrix: any,
   source: any,
   destination: any,
+  // departure: any,
 ) => {
   const modifiedObj = {
     source,
     destination,
     coordinates: matrix,
+    // departure,
   };
   return modifiedObj;
 };
