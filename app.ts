480f446a8ce185c937362ed4123d19dfc228e8a2
Sunny <rajnsunny9@gmail.com>
2024-05-23 09:54:31 +0530

    added socket service

--- app.ts
+++ app.ts
@@ -1,11 +1,19 @@
 import dotenv from 'dotenv';
 if (process.env.NODE_ENV !== 'production') dotenv.config();
 import express, { Express } from 'express';
+import * as http from 'http';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import routes from './src/routes';
+import { Server as SocketIOServer } from 'socket.io';
+import socketService from './src/services/socket-service'
+
+
 
 const app: Express = express();
+const server =  http.createServer(app)
+
+
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(
@@ -16,11 +24,28 @@ app.use(
   }),
 );
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
+
+socketService.initialize(server);
+
 
 app.use(cors());
 app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+setInterval(() => {
+  const flightDetails = {
+      flightNumber: 'XY123',
+      status: 'On Time',
+      departure: '10:00 AM',
+      arrival: '12:00 PM',
+  };
+  socketService.emit('routes',flightDetails);
+}, 5000);
+
+
+
+server.listen(port, () => {
+  console.log(`⚡️[socket]: Socket is running at http://localhost:${port}`);
 });
+
+
