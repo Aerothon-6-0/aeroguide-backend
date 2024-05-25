aefed2274b7827192e5b528d069c85df9108e7bc
Sunny Raj <94435695+rajnsunny@users.noreply.github.com>
2024-05-25 07:39:20 +0530

    Create emergencyTrigger.ts

---
+++ src/routes/v1/emergencyTrigger.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import { EmergencyController } from '../../controllers/emergencyTrigger';
+
+const router = Router();
+
+router.post('/',EmergencyController.addEmergency)
+
+export default router;
