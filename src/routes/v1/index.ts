85dc2e934521971b02e8da5dd8696ec794d38ac0
Sunny Raj <94435695+rajnsunny@users.noreply.github.com>
2024-05-25 07:40:07 +0530

    Update index.ts

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -5,6 +5,7 @@ import flightRoute from './flight';
 import weather from './weather';
 import data from './fetchData';
 import airport from './airport';
+import emergency from './emergencyTrigger';
 
 
 const router = Router();
@@ -18,6 +19,7 @@ router.use('/flight', flightRoute);
 router.use('/weather', weather);
 router.use('/fetchData', data);
 router.use('/airport',airport)
+router.use('/emergency',emergency);
 
 
 export default router;
